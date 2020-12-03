bbed514f61e1121af1026b714c2d6b13a5c49861
haileyringier <haileyringier@outlook.com>
2020-12-03 13:46:39 -0700

    chapter two exercises

--- Listing2-1.py
+++ Listing2-1.py
@@ -5,3 +5,13 @@ room_map = [
     [0, 3, 0, 0, 0],
     [0, 0, 0, 0, 4]
 ]
+
+print(room_map[3][1])
+
+# changing fertilizer to blankets
+room_map[0][0] = 5 
+print(room_map[0][0])
+
+# changing toothpaste to emergency radio
+room_map[4][4] = 6
+print(room_map[4][4])
