b26affe986805cc4702c91096ac6c382c071e55d
haileyringier <haileyringier@outlook.com>
2020-12-04 11:53:41 -0700

    exercises from chapter 4 on loops

---
+++ listing3-3.py
@@ -0,0 +1,26 @@
+room_map = [
+    [1, 1, 1, 1, 1],
+    [1, 0, 0, 0, 1],
+    [1, 0, 1, 0, 1],
+    [1, 0, 0, 0, 1],
+    [1, 0, 0, 0, 1],
+    [1, 0, 0, 0, 1],
+    [1, 1, 1, 1, 1]
+]
+
+# for y in range(7):
+#     print(room_map[y])
+
+# range begins counting at 0 
+
+# chp. 3 - training mission #1 
+for y in range(3):
+    print("Mayday!")
+
+# nested loop 
+
+for y in range(7):
+    for x in range(5):
+        print("y=", y, "x=", x)
+    print()
+
