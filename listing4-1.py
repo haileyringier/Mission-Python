1d21ec0e367fc46300f9a1d97771da70f2920a4f
haileyringier <haileyringier@outlook.com>
2020-12-08 14:35:26 -0700

    adds room generator function from chap 4

--- listing4-1.py
+++ listing4-1.py
@@ -58,4 +58,66 @@ GAME_MAP += [
     ["The robot workshop", 9, 11, True, False] # room 50
 ]
 
+# test to ensure the number of rooms we created matches our map size
 assert len(GAME_MAP) - 1 == MAP_SIZE, "Map size and GAME_MAP do not match!"
+
+#begining of 4-2 
+
+def get_floor_type():
+    if current_room in outdoor_rooms:
+        return 2 # soil 
+    else: 
+        return 0 # tile
+
+def generate_map():
+    global room_map, room_width, room_height, room_name, hazard_map
+    global top_left_x, top_left_y, wall_transparency_frame
+    room_data = GAME_MAP[current_room]
+    room_name = room_data[0]
+    room_height = room_data[1]
+    room_width = room_data[2]
+
+    floor_type = get_floor_type()
+    if current_room in range(1, 21):
+        bottom_edge = 2
+        side_edge = 2
+    if current_room in range(21, 26):
+        bottom_edge = 1
+        side_edge = 2 
+    if current_room > 25:
+        bottom_edge = 1
+        side_edge = 1
+
+    room_map=[[side_edge] * room_width]
+
+    for y in range(room_height - 2):
+        room_map.append([side_edge] + [floor_type]*(room_width - 2) +[side_edge])
+    
+    room_map.append([bottom_edge] * room_width)
+
+    middle_row = int(room_height / 2)
+    middle_column = int(room_width / 2)
+
+    if room_data[4]:
+        room_map[middle_row][room_width - 1] = floor_type
+        room_map[middle_row + 1][room_width - 1] = floor_type
+        room_map[middle_row - 1][room_width - 1] = floor_type
+
+    if current_room % MAP_WIDTH != 1:
+        room_to_left = GAME_MAP[current_room - 1]
+        if room_to_left[4]:
+            room_map[middle_row][0] = floor_type
+            room_map[middle_row + 1][0] = floor_type
+            room_map[middle_row - 1][0] = floor_type
+
+    if room_data[3]:
+        room_map[0][middle_column] = floor_type
+        room_map[0][middle_column + 1] = floor_type
+        room_map[0][middle_column - 1] = floor_type
+
+    if current_room <= MAP_SIZE - MAP_WIDTH:
+        room_below = GAME_MAP[current_room + MAP_WIDTH]
+        if room_below[3]:
+            room_map[room_height - 1][middle_column] = floor_type
+            room_map[room_height - 1][middle_column - 1] = floor_type
+            room_map[room_height - 1][middle_column + 1] = floor_type
