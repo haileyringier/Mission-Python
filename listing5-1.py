b85e1d45f002f2f7af3ab816d3aef2f4d2bf5c7d
haileyringier <haileyringier@outlook.com>
2020-12-09 11:45:41 -0700

    chapter 5 exercise building dictionary

---
+++ listing5-1.py
@@ -0,0 +1,19 @@
+planets = {
+    "Mercury": ["The smallest planet, nearest the Sun", False, 0],
+    "Venus": ["Venus takes 243 days to rotate", False, 0],
+    "Earth": ["The only planet known to have native life", False, 1],
+    "Mars": ["The red planet is the second smallest planet", False, 2],
+    "Jupiter": ["The largest planet, Jupiter is a gas gaint", True, 67],
+    "Saturn": ["The second largest planet is a gas gaint", True, 62],
+    "Uranus": ["An ice giant with a ring system", True, 27],
+    "Neptune": ["An ice gaint and farthest from the sun", True, 14],
+    "Jakku": ["Home to thieves and outlaws", True, 2]
+}
+
+while True: 
+    query = input("Which planet would you like information on? ")
+    if query in planets.keys():
+        print(planets[query][0])
+        print("Does it have rings?", planets[query][1])
+    else: 
+        print("No data available, Sorry!")
